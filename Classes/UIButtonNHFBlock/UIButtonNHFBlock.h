a921a0bac08607cda296f3f158a4bd87339835c5
nhfc99 <nhfc99@163.com>
2018-09-12 18:18:04 +0800

    1.0.0

--- Classes/UIButtonNHFBlock/UIButtonNHFBlock.h
+++ Classes/UIButtonNHFBlock/UIButtonNHFBlock.h
@@ -11,7 +11,7 @@
 typedef void (^NHFActionBlock)(void);
 
 //@interface UIButton (NHFBlock)
-@interface UIButtonNHFBlock
+@interface UIButtonNHFBlock : NSObject 
 
 //@property (readonly) NSMutableDictionary *event;
 //
