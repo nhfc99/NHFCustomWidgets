dbbe6b2b6ac590709001103adabe13dd60a41fee
nhfc99 <nhfc99@163.com>
2018-11-22 11:47:05 +0800

    1.0.26

--- Classes/NHFSynService/NHFSynService.h
+++ Classes/NHFSynService/NHFSynService.h
@@ -8,8 +8,33 @@
 
 #import <UIKit/UIKit.h>
 
+/***********
+ **说明：NHFSynService为核心，用于实现分发操作机制，如果要实现分发机制需要自己在外层进行编写一个单例，如果实现多对一的功能可以直接使用该类，对外出口全部是NHFSynService
+ **
+ **
+ */
+
 NS_ASSUME_NONNULL_BEGIN
 
+@class NHFSynService;
+
+//缓存操作对象
+@interface NHFSynServiceCache : NSObject
+
++ (instancetype)getInstancetype;
+
+//添加指定的对象
+- (void)addObject:(NHFSynService *)object keyString:(NSString *)keyString;
+
+//移除指定的键名
+- (void)removeKeyString:(NSString *)keyString;
+
+//获取对象
+- (NHFSynService *)objectForKeyString:(NSString *)keyString;
+
+@end
+
+//存储操作数据对象
 @interface NHFWeakServiceObject : NSObject
 
 @property (nonatomic, weak) id weakObject;
@@ -18,7 +43,6 @@ NS_ASSUME_NONNULL_BEGIN
 @end
 
 @interface NHFSynService : NSObject
-
 
 /**
  添加对象和方法
@@ -47,9 +71,26 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  执行更新操作 - 指定的方法
 
- @param selString
+ @param selString 不可以为空
  */
 - (void)updateObjectBySel:(NSString *)selString;
+
+
+/**
+ 保存该对象
+
+ @param key --
+ */
+- (void)saveServiceKey:(NSString *)key;
+
+
+/**
+ 获取指定对象
+
+ @param key --
+ @return --
+ */
++ (NHFSynService *)serviceKey:(NSString *)key;
 
 @end
 
--- Classes/NHFSynService/NHFSynService.m
+++ Classes/NHFSynService/NHFSynService.m
@@ -8,6 +8,63 @@
 
 #import "NHFSynService.h"
 
+static NHFSynServiceCache *_nhfSynServiceCache = nil;
+
+@interface NHFSynServiceCache ()
+
+@property (nonatomic, retain) NSMutableDictionary *cacheService;
+
+@end
+
+@implementation NHFSynServiceCache
+
++ (instancetype)getInstancetype {
+    static dispatch_once_t predicate;
+    dispatch_once(&predicate, ^{
+        _nhfSynServiceCache = [NHFSynServiceCache new];
+    });
+    return _nhfSynServiceCache;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _cacheService = [NSMutableDictionary new];
+    }
+    return self;
+}
+
+//添加指定的对象
+- (void)addObject:(NHFSynService *)object keyString:(NSString *)keyString {
+    if (object == nil ||
+        keyString == nil) {
+        return;
+    }
+    //保存对象
+    NHFWeakServiceObject *jhWeakObject = [NHFWeakServiceObject new];
+    jhWeakObject.weakObject = object;
+    [_cacheService removeObjectForKey:keyString];
+    [_cacheService setObject:jhWeakObject forKey:keyString];
+}
+
+- (NHFSynService *)objectForKeyString:(NSString *)keyString {
+    //判断是否为空，如果为空了就清除
+    NHFWeakServiceObject *jhWeakObject = [_cacheService objectForKey:keyString];
+    if (jhWeakObject.weakObject) {
+        return jhWeakObject.weakObject;
+    }
+    [self removeKeyString:keyString];
+    return nil;
+}
+
+//移除指定的键名
+- (void)removeKeyString:(NSString *)keyString {
+    [_cacheService removeObjectForKey:keyString];
+}
+
+@end
+
 @implementation NHFWeakServiceObject
 
 @end
@@ -100,6 +157,15 @@
     @synchronized (self) {
         [_objects removeObject:jhWeakObject];
     }
+}
+
+#pragma mark - 存储本对象，用于其他地方随时使用
+- (void)saveServiceKey:(NSString *)key {
+    [[NHFSynServiceCache getInstancetype] addObject:self keyString:key];
+}
+
++ (NHFSynService *)serviceKey:(NSString *)key {
+    return [[NHFSynServiceCache getInstancetype] objectForKeyString:key];
 }
 
 @end
